362eaf57ea0fb07e4477b32fa951bb680dfd060f
Thalles Carneiro <thallescarneiro@usp.br>
2022-10-10 17:18:10 -0300

    Algumas coisas de plotter

---
+++ Plotter/serial_sender/serial_sender.ino
@@ -0,0 +1,67 @@
+unsigned long timer = 0;
+long loopTime = 5000;   // microseconds
+
+void setup() {
+  Serial.begin(38400);
+  timer = micros();
+}
+
+void loop() {
+  timeSync(loopTime);
+  //int val = analogRead(0) - 512;
+  double val = (analogRead(0) -512) / 512.0;
+  sendToPC(&val,&val+1,&val+2);
+}
+
+void timeSync(unsigned long deltaT)
+{
+  unsigned long currTime = micros();
+  long timeToDelay = deltaT - (currTime - timer);
+  if (timeToDelay > 5000)
+  {
+    delay(timeToDelay / 1000);
+    delayMicroseconds(timeToDelay % 1000);
+  }
+  else if (timeToDelay > 0)
+  {
+    delayMicroseconds(timeToDelay);
+  }
+  else
+  {
+      // timeToDelay is negative so we start immediately
+  }
+  timer = currTime + timeToDelay;
+}
+
+void sendToPC(int* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 2);
+}
+
+void sendToPC(double* data)
+{
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData, 4);
+}
+void sendToPC(int* data1, int* data2, int* data3)
+{
+  byte* byteData1 = (byte*)(data1);
+  byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);
+  byte buf[6] = {byteData1[0], byteData1[1],
+                 byteData2[0], byteData2[1],
+                 byteData3[0], byteData3[1]};
+  Serial.write(buf, 6);
+}
+
+void sendToPC(double* data1, double* data2, double* data3)
+{
+  byte* byteData1 = (byte*)(data1);
+  byte* byteData2 = (byte*)(data2);
+  byte* byteData3 = (byte*)(data3);
+  byte buf[12] = {byteData1[0], byteData1[1], byteData1[2], byteData1[3],
+                 byteData2[0], byteData2[1], byteData2[2], byteData2[3],
+                 byteData3[0], byteData3[1], byteData3[2], byteData3[3]};
+  Serial.write(buf, 12);
+}
